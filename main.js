0cd8432d1bcc8fdb5d4ee35aef4623968874ee5c
jahc07 <jahcands@gmail.com>
2019-09-26 18:07:36 -0400

    26 de sep

--- main.js
+++ main.js
@@ -31,10 +31,74 @@ var Rectangle = /** @class */ (function () {
     Rectangle.prototype.getwidth = function () {
         return this.width;
     };
+    Rectangle.prototype.getArea = function () {
+        var area = this.height + this.width;
+        return area;
+    };
     return Rectangle;
 }());
 exports.Rectangle = Rectangle;
 var Rectangle1 = new Rectangle(7, 5, 3, 7);
 Rectangle1.setposX(150);
-console.log(Rectangle1.getposX());
 console.log(Rectangle1);
+console.log(Rectangle1.getposX());
+var Squard = /** @class */ (function () {
+    function Squard(px, py) {
+        this.posX = px;
+        this.posY = py;
+    }
+    Squard.prototype.setposX = function (px) {
+        this.posX = px;
+    };
+    Squard.prototype.getposX = function () {
+        return this.posX;
+    };
+    Squard.prototype.setposY = function (py) {
+        this.posY = py;
+    };
+    Squard.prototype.getposY = function () {
+        return this.posY;
+    };
+    return Squard;
+}());
+exports.Squard = Squard;
+var Squard1 = new Squard(10, 5);
+Squard1.setposX(60);
+console.log(Squard1);
+var Circle = /** @class */ (function () {
+    function Circle() {
+    }
+    Circle.prototype.contructor = function (r) {
+        this.radio = r;
+    };
+    Circle.prototype.setradio = function (r) {
+    };
+    Circle.prototype.getradio = function () {
+        return this.radio;
+    };
+    return Circle;
+}());
+exports.Circle = Circle;
+var Circle1 = new Circle();
+Circle1.setradio(24);
+console.log(Circle1);
+var Sector = /** @class */ (function () {
+    function Sector(ax, ry) {
+        this.angX = ax;
+        this.radY = ry;
+    }
+    Sector.prototype.getangX = function () {
+        return this.angX;
+    };
+    Sector.prototype.setradY = function (ry) {
+        this.radY = ry;
+    };
+    Sector.prototype.getradY = function () {
+        return this.radY;
+    };
+    return Sector;
+}());
+exports.Sector = Sector;
+var Sector1 = new Sector(12, 34);
+Sector1.setradY(40);
+console.log(Sector1);
--- main.ts
+++ main.ts
@@ -34,10 +34,97 @@ export class Rectangle {
     public getwidth(): number {
         return this.width;
     }
+    public getArea(): number {
+        let area: number = this.height + this.width;
+        return area;
+    }
+    //  public getDesplazar(): number {
+    // let desplazar: number = this.width;//
 }
+
+
+
 let Rectangle1: Rectangle = new Rectangle(7, 5, 3, 7);
 
-Rectangle1.setposX (150)
-console.log(Rectangle1.getposX());
+Rectangle1.setposX(150)
 console.log(Rectangle1);
+console.log(Rectangle1.getposX());
+
+export class Squard {
+    private posX: number;
+    private posY: number;
+
+    public constructor(px, py) {
+        this.posX = px;
+        this.posY = py;
+    }
+    public setposX(px: number): void {
+        this.posX = px;
+    }
+    public getposX(): number {
+        return this.posX;
+
+    } public setposY(py: number): void {
+        this.posY = py;
+    }
+    public getposY(): number {
+        return this.posY;
+    }
+}
+let Squard1: Squard = new Squard(10, 5);
+Squard1.setposX(60)
+console.log(Squard1);
+
+export class Circle {
+    private radio: number;
+    contructor(r: number) {
+        this.radio = r;
+    }
+    public setradio(r: number): void {
+
+    }
+    public getradio(): number {
+        return this.radio;
+    }
+}
+let Circle1: Circle = new Circle();
+Circle1.setradio(24)
+console.log(Circle1);
+
+export class Sector {
+    private angX: number;
+    private radY: number;
+
+    public constructor(ax, ry) {
+        this.angX = ax;
+        this.radY = ry;
+    }
+    public getangX(): number {
+        return this.angX;
+
+    } public setradY(ry: number): void {
+        this.radY = ry;
+    }
+    public getradY(): number {
+        return this.radY;
+    }
+}
+let Sector1: Sector = new Sector (12, 34);
+Sector1.setradY(40)
+console.log(Sector1);
+
+export class banco{
+    private ingresoX: number;
+    private retiroX: number;
+    private saldoX: number;
+    
+    public constructor(in: number, re: number, sa: number){
+        this.ingresoX = in;
+        this.retiroX = re;
+        this.saldoX = sa;
+    }
+} 
+
+
+
 
